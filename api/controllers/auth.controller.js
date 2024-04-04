640014c2b3cde08388daf743379acdcd84c0433f
kishore <kishoreram10106@gmail.com>
2024-04-04 19:58:17 +0530

    add functionality to my signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,6 +16,7 @@ export const signup = async (req,res,next)=>{
     try{
         await newUser.save();
         res.json('Signup Successful');
+        
     }catch(error){
         next(error);
     }
