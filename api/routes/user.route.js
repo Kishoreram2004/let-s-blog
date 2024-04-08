b796bb02ea63e937633986479d6f960aad6f42c6
kishore <kishoreram10106@gmail.com>
2024-04-08 20:37:10 +0530

    show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { test, updateUser, deleteUser, signout } from "../controllers/user.controller.js";
+import {
+    deleteUser,
+    getUser,
+    signout,
+    test,
+    updateUser,
+  } from '../controllers/user.controller.js';
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +13,6 @@ router.get('/test',test);
 router.put('/update/:userId', verifyToken ,updateUser)
 router.delete("/delete/:userId",verifyToken, deleteUser)
 router.post("/signout", signout)
+router.get('/:userId', getUser);
 
 export default router;  
